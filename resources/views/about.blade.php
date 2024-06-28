e48882fcb30c3e8cbb16c8000e3dbff1a6a8746e
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2024-06-29 00:51:17 +0700

    Belajar Laravel 11 | 5. Blade Component

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">This is About Us!</h3>
+    <h3>Hi! My name is {{ $name }}</h3>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Rizal Baihaqi', 'title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
